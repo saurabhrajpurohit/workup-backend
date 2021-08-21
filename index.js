5ca368a2474fa782677762ca0f34b86c13b5fda0
Saurabh <purohitsraj52@gmail.com>
2021-08-21 20:43:57 +0530

    Updated port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/airbnb", airbnb)
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("App listening at http://%s:%s", host, port)
